b48868dcc5a8179f62f1eb335ad9a589d5985e10
vaibhavhirani <vaibhavhirani@gmail.com>
2023-07-27 01:31:40 +0530

    creates public & private subnets

--- modules/my_eks_cluster/main.tf
+++ modules/my_eks_cluster/main.tf
@@ -22,4 +22,63 @@ resource "aws_internet_gateway" "eks_ig" {
 
 
 # creates  2 private and 2 public subnets 
-re
+resource "aws_subnet" "eks_public_sb_1" {
+    vpc_id = aws_vpc.eks_vpc.id  
+    cidr_block = "192.168.0.0/18"
+    availability_zone = "ap-south-1a"
+    #every instance deployed will get a public ip
+    map_public_ip_on_launch = true 
+     tags = {
+        Name = format("%s-%s-%s","public", var.tag, "1")
+        #below tags allows eks cluster to share the subnet & lets you deploy public elb
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/elb" = 1
+    }        
+
+}
+
+resource "aws_subnet" "eks_public_sb_2" {
+    vpc_id = aws_vpc.eks_vpc.id  
+    cidr_block = "192.168.64.0/18"
+    availability_zone = "ap-south-1b"
+    #every instance deployed will get a public ip
+    map_public_ip_on_launch = true 
+     tags = {
+        Name = format("%s-%s-%s","public", var.tag, "2")
+        #below tags allows eks cluster to share the subnet & lets you deploy public elb
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/elb" = 1
+    }        
+
+}
+
+# creates  2 private and 2 public subnets 
+resource "aws_subnet" "eks_private_sb_1" {
+    vpc_id = aws_vpc.eks_vpc.id  
+    cidr_block = "192.168.128.0/18"
+    availability_zone = "ap-south-1a"
+    #every instance deployed will get a public ip
+    # map_public_ip_on_launch = true 
+     tags = {
+        Name = format("%s-%s-%s","private", var.tag, "1")
+        #below tags allows eks cluster to share the subnet & lets you deploy private elb
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/internal-elb" = 1
+    }        
+
+}
+
+resource "aws_subnet" "eks_private_sb_2" {
+    vpc_id = aws_vpc.eks_vpc.id  
+    cidr_block = "192.168.192.0/18"
+    availability_zone = "ap-south-1b"
+    #every instance deployed will get a public ip
+    # map_public_ip_on_launch = true 
+     tags = {
+        Name = format("%s-%s-%s","private", var.tag, "1")
+        #below tags allows eks cluster to share the subnet & lets you deploy private elb
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/internal-elb" = 1
+    }        
+
+}
--- modules/my_eks_cluster/variables.tf
+++ modules/my_eks_cluster/variables.tf
@@ -1,5 +1,5 @@
 variable "vpc_cidr_block" {
-    default = "10.0.0.0/16"
+    default = "192.168.0.0/16"
 }
 
 variable "tag" {
